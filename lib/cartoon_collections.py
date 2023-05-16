1ec312280245be17a681a61c30da78d3a9dcef4b
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-17 00:31:34 +0300

    Completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for index, name in enumerate(names, start=1):
+        print(f"{index}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(names):
+    return [(name.capitalize() + '!') for name in names]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    for word in list:
+        if (word == "cheddar") or (word == "cheddar") or (word == "cheddar"):
+            return word
+    return None
